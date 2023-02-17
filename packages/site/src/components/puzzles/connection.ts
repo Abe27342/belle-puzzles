fecd0aa08b66c7f3b5a004725d72febefa35d477
Abram Sanderson <abram.sanderson@gmail.com>
2023-02-17 14:20:04 -0800

    restore local connection flow, which is at least partially right. and hey, it can tree-shake now!

--- packages/site/src/components/puzzles/connection.ts
+++ packages/site/src/components/puzzles/connection.ts
@@ -5,24 +5,39 @@ import {
 	AzureMember,
 	AzureRemoteConnectionConfig,
 } from '@fluidframework/azure-client';
-import { InsecureTokenProvider } from '@fluidframework/test-client-utils';
-
-const FLUID_CONNECTION_TYPE = 'remote';
 
 export async function getConnection(
 	user?: Pick<AzureMember, 'userId' | 'userName' | 'additionalDetails'>
 ): Promise<AzureLocalConnectionConfig | AzureRemoteConnectionConfig> {
-	// Note: This doesn't actually tree shake correctly. I confirmed that local code imported here
-	// is tree shaking correctly, so it might be related to output format of test-runtime-utils.
-	// This structure at least has a chance of doing so, though.
+	if (import.meta.env.VITE_FLUID_CONNECTION_TYPE === 'local') {
+		const { InsecureTokenProvider } = await import(
+			'@fluidframework/test-client-utils'
+		);
+		const localConnection: AzureLocalConnectionConfig = {
+			type: 'local',
+			tokenProvider: new InsecureTokenProvider('', {
+				id: 'local user id',
+			}),
+			endpoint: 'http://localhost:7070',
+		};
 
-	return {
-		type: 'remote',
-		tenantId: '3ba404d1-7aff-4082-b3f8-794e6c6ae63e',
-		// TODO: Docs should mention that this TokenProvider is exported from @fluidframework/azure-client.
-		tokenProvider: new AzureFunctionTokenProvider('/api/getToken', user),
-		endpoint: 'https://us.fluidrelay.azure.com',
-	};
+		return localConnection;
+	} else if (import.meta.env.VITE_FLUID_CONNECTION_TYPE === 'remote') {
+		return {
+			type: 'remote',
+			tenantId: '3ba404d1-7aff-4082-b3f8-794e6c6ae63e',
+			// TODO: Docs should mention that this TokenProvider is exported from @fluidframework/azure-client.
+			tokenProvider: new AzureFunctionTokenProvider(
+				'/api/getToken',
+				user
+			),
+			endpoint: 'https://us.fluidrelay.azure.com',
+		};
+	}
+
+	throw new Error(
+		'Attempted to create fluid connection but no type specified. This indicates a webpack issue.'
+	);
 }
 
 let configuredConnection:
