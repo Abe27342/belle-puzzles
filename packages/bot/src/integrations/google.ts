3f5f862e2baa6e1f2f69ba0c636ad722f344b109
Abram Sanderson <Abram.Sanderson@microsoft.com>
2024-03-15 15:57:12 -0700

    Lazily initialize google service auth (#28)

--- packages/bot/src/integrations/google.ts
+++ packages/bot/src/integrations/google.ts
@@ -1,16 +1,21 @@
 import '../register-env/index.js';
 import { google } from 'googleapis';
 
-const auth = new google.auth.GoogleAuth({
-	credentials: JSON.parse(process.env.GOOGLE_API_KEY),
-	scopes: ['https://www.googleapis.com/auth/drive.file'],
-});
-google.options({ auth });
+let isInitialized = false;
 
 export const createGoogleSheet = async (
 	name: string,
 	googleFolderId: string
 ) => {
+	if (!isInitialized) {
+		// Do this lazily to allow module load in environments that don't have access to the google api key (ex: command deployment)
+		const auth = new google.auth.GoogleAuth({
+			credentials: JSON.parse(process.env.GOOGLE_API_KEY),
+			scopes: ['https://www.googleapis.com/auth/drive.file'],
+		});
+		google.options({ auth });
+		isInitialized = true;
+	}
 	const drive = google.drive({ version: 'v3' });
 	const file = await drive.files.copy({
 		fileId: GOOGLE_SHEET_TEMPLATE_ID,
