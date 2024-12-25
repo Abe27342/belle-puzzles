ea0a47a8a81a84d65a10aeceae397caacb9b6a5f
Abram Sanderson <Abram.Sanderson@microsoft.com>
2024-12-24 23:58:12 -0600

    Add /populate_answers (#33)
    Adds some functionality which should be useful for metas.
    
    When /populate_answers <round channel> is run from a puzzle channel, a tab (sheet) will be added to that puzzle's spreadsheet containing all feeder answers from puzzles which are descendants of the specified round.

--- packages/bot/src/integrations/google.ts
+++ packages/bot/src/integrations/google.ts
@@ -1,21 +1,15 @@
 import '../register-env/index.js';
-import { google } from 'googleapis';
-
-let isInitialized = false;
+import { google, type sheets_v4 } from 'googleapis';
 
 export const createGoogleSheet = async (
 	name: string,
 	googleFolderId: string
 ) => {
-	if (!isInitialized) {
-		// Do this lazily to allow module load in environments that don't have access to the google api key (ex: command deployment)
-		const auth = new google.auth.GoogleAuth({
-			credentials: JSON.parse(process.env.GOOGLE_API_KEY),
-			scopes: ['https://www.googleapis.com/auth/drive.file'],
-		});
-		google.options({ auth });
-		isInitialized = true;
-	}
+	const auth = new google.auth.GoogleAuth({
+		credentials: JSON.parse(process.env.GOOGLE_API_KEY),
+		scopes: ['https://www.googleapis.com/auth/drive.file'],
+	});
+	google.options({ auth });
 	const drive = google.drive({ version: 'v3' });
 	const file = await drive.files.copy({
 		fileId: GOOGLE_SHEET_TEMPLATE_ID,
@@ -32,3 +26,58 @@ export const createGoogleSheet = async (
 // Since Belle Bot only gets drive.file though, the flow might be somewhat janky.
 export const GOOGLE_SHEET_TEMPLATE_ID =
 	'1IqR4EcKA7gkxvveIzPCgcbcWUozMbsQwDXbKcQaPH9U';
+
+export async function updateSheetValues(
+	params: sheets_v4.Params$Resource$Spreadsheets$Values$Batchupdate
+): Promise<void> {
+	const auth = new google.auth.GoogleAuth({
+		credentials: JSON.parse(process.env.GOOGLE_API_KEY),
+		scopes: ['https://www.googleapis.com/auth/drive.file'],
+	});
+	google.options({ auth });
+	const sheets = google.sheets({ version: 'v4' });
+	const response = await sheets.spreadsheets.values.batchUpdate(params);
+
+	if (response.status !== 200) {
+		console.log(response);
+		throw new Error(`Issue updating data: ${response.statusText}`);
+	}
+}
+
+export async function updateSheet(
+	params: sheets_v4.Params$Resource$Spreadsheets$Batchupdate
+): Promise<sheets_v4.Schema$BatchUpdateSpreadsheetResponse> {
+	const auth = new google.auth.GoogleAuth({
+		credentials: JSON.parse(process.env.GOOGLE_API_KEY),
+		scopes: ['https://www.googleapis.com/auth/drive.file'],
+	});
+	google.options({ auth });
+	const sheets = google.sheets({ version: 'v4' });
+	const response = await sheets.spreadsheets.batchUpdate(params);
+
+	if (response.status !== 200) {
+		console.log(response);
+		throw new Error(`Issue updating data: ${response.statusText}`);
+	}
+
+	return response.data;
+}
+
+export async function getByDataFilter(
+	params: sheets_v4.Params$Resource$Spreadsheets$Getbydatafilter
+): Promise<sheets_v4.Schema$Spreadsheet> {
+	const auth = new google.auth.GoogleAuth({
+		credentials: JSON.parse(process.env.GOOGLE_API_KEY),
+		scopes: ['https://www.googleapis.com/auth/drive.file'],
+	});
+	google.options({ auth });
+	const sheets = google.sheets({ version: 'v4' });
+	const response = await sheets.spreadsheets.getByDataFilter(params);
+
+	if (response.status !== 200) {
+		console.log(response);
+		throw new Error(`Issue updating data: ${response.statusText}`);
+	}
+
+	return response.data;
+}
