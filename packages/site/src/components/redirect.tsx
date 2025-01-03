2dc52220f237d7d3cf07c03c5cd801a4efd260a3
Abram Sanderson <Abram.Sanderson@microsoft.com>
2025-01-02 17:48:34 -0800

    Simplify auth redirects and restore post-login path functionality (#36)

--- packages/site/src/components/redirect.tsx
+++ packages/site/src/components/redirect.tsx
@@ -1,31 +1,39 @@
 import * as React from 'react';
-import { Navigate, Outlet, useSearchParams } from 'react-router-dom';
+import {
+	Navigate,
+	Outlet,
+	useLocation,
+	useNavigate,
+	useSearchParams,
+} from 'react-router-dom';
 import { getPostRedirectPath, setPostRedirectPath } from '../redirect';
-
-export const RootRedirect: React.FC = () => {
-	const [params] = useSearchParams();
-	const pathname = getPostRedirectPath();
-	if (pathname) {
-		setPostRedirectPath(null);
-		return <Navigate to={`${pathname}?${params}`} replace />;
-	}
-
-	return <Navigate to={`/home?${params}`} replace />;
-};
 
 // OAuth2 redirect URIs include these but they are not generally useful after consumption.
 // Though we consume these at the top-level in a loader, it's easiest to remove them inside a react component
 // (setting window.location.search works but triggers extra page reloads)
 const paramsToRemove = ['code', 'state', 'guild_id', 'permissions'];
-export const RemoveAuthParams: React.FC = () => {
-	const [search, setSearch] = useSearchParams();
+export const HandleAuthRedirects: React.FC = () => {
+	const { pathname } = useLocation();
+	const [search] = useSearchParams();
+	const navigate = useNavigate();
 	React.useEffect(() => {
 		if (paramsToRemove.some((param) => search.has(param))) {
 			for (const param of paramsToRemove) {
 				search.delete(param);
 			}
-			setSearch(search);
 		}
-	}, [search]);
+
+		// If we're being redirected back to belle-puzzles from an OAuth2 flow,
+		// redirect to the post-login path we stashed in local state (and clear it) if it exists.
+		const redirectPath = getPostRedirectPath();
+		setPostRedirectPath(null);
+		navigate(
+			{
+				pathname: redirectPath ?? pathname,
+				search: `${search}`,
+			},
+			{ replace: true }
+		);
+	}, [search, navigate]);
 	return <Outlet />;
 };
