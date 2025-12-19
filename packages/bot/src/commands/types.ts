cd94222b36cde2d447c9db0db0665a1b50cbf92c
Abram Sanderson <absander@microsoft.com>
2025-12-19 14:08:51 -0800

    Build fixes after merging

--- packages/bot/src/commands/types.ts
+++ packages/bot/src/commands/types.ts
@@ -1,13 +1,19 @@
 import type {
 	SlashCommandBuilder,
 	ChatInputCommandInteraction,
+	SlashCommandOptionsOnlyBuilder,
 	SlashCommandSubcommandsOnlyBuilder,
 } from 'discord.js';
 import type { PuzzlehuntContext } from '../puzzlehunt-context';
 
+export type SlashCommandData =
+	| SlashCommandBuilder
+	| SlashCommandOptionsOnlyBuilder
+	| SlashCommandSubcommandsOnlyBuilder;
+
 export interface Command {
 	requiresSerializedContext?: boolean; // defaults to true
-	data: SlashCommandBuilder | SlashCommandSubcommandsOnlyBuilder;
+	data: SlashCommandBuilder | SlashCommandData;
 	testServerOnly?: boolean; // defaults to false
 	adminOnly?: boolean; // defaults to false
 	execute: (
